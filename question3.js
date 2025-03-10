29b630dc3d842b164cd07f80882fd8d1228ab820
unknown <YY231@ccs.local>
2025-03-10 06:08:55 -0700

    question3.js

--- question3.js
+++ question3.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const app = express();
+const app = express();            
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Express is working! Mendoza, Perry Ian A.')
+    res.send('Express is working! Mendoza, Perry Ian A.')      //method to send or request response
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`)
+    console.log(`Server is running at http://localhost:${port}`)        //method to start the server
 });
